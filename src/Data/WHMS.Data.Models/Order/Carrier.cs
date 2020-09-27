5461eede6736873ae607bb8a61e7a52a27dbaf93
Живко Писков <zpiskov@uni-sofia.bg>
2020-09-27 17:05:40 +0300

    fix carrier/shipping method max lenght

--- src/Data/WHMS.Data.Models/Order/Carrier.cs
+++ src/Data/WHMS.Data.Models/Order/Carrier.cs
@@ -1,11 +1,13 @@
 ﻿namespace WHMS.Data.Models.Order
 {
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     using WHMS.Data.Common.Models;
 
     public class Carrier : BaseDeletableModel<int>
     {
+        [MaxLength(20)]
         public string Name { get; set; }
 
         public ICollection<ShippingMethod> ShippingMethods { get; set; }
--- src/Data/WHMS.Data.Models/Order/ShippingMethod.cs
+++ src/Data/WHMS.Data.Models/Order/ShippingMethod.cs
@@ -1,11 +1,14 @@
 ﻿namespace WHMS.Data.Models.Order
 {
+    using System.ComponentModel.DataAnnotations;
+
     using WHMS.Data.Common.Models;
 
     public class ShippingMethod : BaseDeletableModel<int>
     {
         public Carrier Carrier { get; set; }
 
+        [MaxLength(50)]
         public string Name { get; set; }
     }
 }
